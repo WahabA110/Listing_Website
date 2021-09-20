cbc17e3aba17888682325a3917af5808bd6fd941
huongw <whitneyhuong@gmail.com>
2021-09-20 06:33:23 +0000

    remove comment

--- 1_queries/admin_insert_product.sql
+++ 1_queries/admin_insert_product.sql
@@ -1,4 +1,2 @@
 INSERT INTO products (id, user_id, name, price, description, thumbnail_photo)
 VALUES (9, 1, 'Montellier', 20000, 'description', 'https://image.shutterstock.com/image-illustration/white-living-room-sofa-scandinavian-600w-1672306783.jpg');
-
-/* let mentor check */
