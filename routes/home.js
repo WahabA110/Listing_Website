f9755ba46e5b2eeebb98c02761f213ea4d4badba
Yirong Qin <yq4103@mun.ca>
2021-09-23 12:19:22 -0400

    sort routes added

--- routes/home.js
+++ routes/home.js
@@ -50,7 +50,56 @@ module.exports = (db) => {
         .json({ error: err.message });
     });
 
-  })
+  });
+
+  router.get("/DESC", (req, res) => {
+
+    const queryString = `SELECT name, description, thumbnail_photo, price
+    FROM products
+    WHERE sold_date IS NULL
+    ORDER BY price DESC;`;
+
+    db.query(queryString)
+      .then(data => {
+        const products = data.rows;
+        console.log(data.rows)
+        const templateVars = {
+          products
+        };
+
+        res.render("index", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+
+  router.get("/ASC", (req, res) => {
+
+    const queryString = `SELECT name, description, thumbnail_photo, price
+    FROM products
+    WHERE sold_date IS NULL
+    ORDER BY price;`;
+
+    db.query(queryString)
+      .then(data => {
+        const products = data.rows;
+        console.log(data.rows)
+        const templateVars = {
+          products
+        };
+
+        res.render("index", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
   return router;
 };
