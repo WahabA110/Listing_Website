2060d7110270ddfa20f85c7df0dd2476a5cbd9bd
Wahab Alamin <wahabalamin@gmail.com>
2021-09-23 15:55:21 -0400

    implemented post route for message seller button. Users can now message the seller/admin regarding specific products on the homepage or in their favorites page

--- routes/home.js
+++ routes/home.js
@@ -28,7 +28,7 @@ module.exports = (db) => {
       });
   });
 
-  router.post("/favorites/:id", (req, res) => {
+  router.post("/:id", (req, res) => {
 
     const queryString = `
     INSERT INTO favorites (user_id, product_id)
@@ -87,7 +87,6 @@ module.exports = (db) => {
     db.query(queryString)
       .then(data => {
         const products = data.rows;
-        console.log(data.rows)
         const templateVars = {
           products
         };
@@ -100,6 +99,55 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post("/conversations/:id", (req, res) => {
+
+    const userId = req.session.user_id;
+    const product = req.params.id;
+
+    const queryString = `
+    SELECT user_id
+    FROM products
+    WHERE id = $1
+    `;
+
+    const queryString2 = `
+    INSERT INTO conversations (product_id, user_id, seller_id)
+    VALUES ($1, $2, $3) RETURNING *
+    `;
+
+    const queryString3 = `
+    INSERT INTO messages (message, conversation_id, sender_id)
+    VALUES ('Im interested in this product.', $1, $2)
+    `;
+
+    const val = [product]
+
+
+    db.query(queryString, val)
+    .then(data => {
+      const info = data.rows;
+      const user = info[0].user_id;
+
+      const val2 = [product, userId, user];
+
+      return db.query(queryString2, val2);
+    })
+    .then(data => {
+      const convo = data.rows;
+      const id = convo[0].id;
+
+      const val3 = [id, userId];
+
+      return db.query(queryString3, val3);
+    })
+    .then(() => {
+      res.redirect("/conversations");
+    })
+    .catch(err => {
+      console.log("err:", err);
+    })
+  })
 
   return router;
 };
